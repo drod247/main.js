add4c65be2ea8f7af0781f05cf259f554904dddc
David Rodriguez <24427021+drod247@users.noreply.github.com>
2021-12-22 18:32:02 -0500

    rename variables

--- update.php
+++ update.php
@@ -10,9 +10,9 @@ if ( file_exists( './script/' . $main ) ) {
         unlink( './script/' . $main);
 
 
-                $js = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/main.js');
+                $components = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/main.js');
                 $myfile = fopen('./script/'. $main, "w") or die("Unable to open $main!");
-                fwrite($myfile, $js);
+                fwrite($myfile, $components);
                 fclose($myfile);
         
 }
@@ -24,9 +24,9 @@ if ( file_exists( './' . $app ) ) {
         unlink( './' . $app);
 
          
-        $js = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/app.php');
+        $library = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/app.php');
                  $myfile = fopen('./'. $app, "w") or die("Unable to open $app!");
-                 fwrite($myfile, $js);
+                 fwrite($myfile, $library);
                  fclose($myfile);
 }
 
@@ -38,9 +38,9 @@ if ( file_exists( './css/' . $css ) ) {
         unlink( './css/' . $css);
 
                  
-        $js = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/vsdg.css');
+        $style = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/vsdg.css');
                 $myfile = fopen('./css/'. $css, "w") or die("Unable to open $css!");
-                fwrite($myfile, $js);
+                fwrite($myfile, $style);
                 fclose($myfile);
                          
 }
