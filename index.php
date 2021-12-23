5f9bbd5f907b5898e315e438b7e99ebf893667ed
David Rodriguez <24427021+drod247@users.noreply.github.com>
2021-12-23 00:12:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 
 <?php
-$pageURL = (@$_SERVER["HTTPS"] == "on") ? "https://" : "http://";
-if ($_SERVER["SERVER_PORT"] != "80")
-{
-    $pageURL .= substr($_SERVER['SERVER_NAME'], 4).":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-} 
-else 
-{
-    $pageURL .= substr($_SERVER['SERVER_NAME'], 4).$_SERVER["REQUEST_URI"];
-}
-return $pageURL;
-    $_dir_ = $_SERVER['DOCUMENT_ROOT'];
-    $tokens = explode(".", $_SERVER['HTTP_HOST']);
-    $domain = $tokens[0];
+
+$_dir_ = $_SERVER['DOCUMENT_ROOT'];
+$tokens = explode(".", $_SERVER['HTTP_HOST']);
+$domain = $tokens[0];
+
+
+
+$urlParts = parse_url($domain);
+
+// remove www
+$domain = preg_replace('/^www\./', '', $urlParts['host']);
+
+
 if ( file_exists( './'.$domain.'.php') ) {
-    
-    require_once($_dir_.$domain.'.php');
+
+require_once($_dir_.$domain.'.php');
 } else {
 
 
-        $j = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/app.php');
-        $myfile = fopen('./'. $domain.'.php', "w") or die("Unable to open $domain.'.php'!");
-        fwrite($myfile, $j);
-        fclose($myfile);
-    require_once($_dir_.$domain.'.php');
-        
-        }
+    $j = file_get_contents('https://raw.githubusercontent.com/drod247/main.js/main/app.php');
+    $myfile = fopen('./'. $domain.'.php', "w") or die("Unable to open $domain.'.php'!");
+    fwrite($myfile, $j);
+    fclose($myfile);
+require_once($_dir_.$domain.'.php');
+    
+    }
 
 
 
-    ?>
+?>
